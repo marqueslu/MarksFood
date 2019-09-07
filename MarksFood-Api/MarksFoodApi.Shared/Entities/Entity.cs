d6341ba7a852baa1ed77f30f19167578347dd05f
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-09-07 16:17:10 -0300

    Criação da camada shared para itens que serão de uso geral

---
+++ MarksFood-Api/MarksFoodApi.Shared/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarksFoodApi.Shared.Entities
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+    }
+}
