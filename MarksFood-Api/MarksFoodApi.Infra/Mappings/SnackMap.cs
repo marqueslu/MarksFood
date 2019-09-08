9f6e753fd9926b92243bfc18582e8ad7722c0c11
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-09-08 16:45:58 -0300

    Adição do projeto de infra e configurações relacionadas ao EF Core

---
+++ MarksFood-Api/MarksFoodApi.Infra/Mappings/SnackMap.cs
@@ -0,0 +1,20 @@
+﻿using MarksFoodApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarksFoodApi.Infra.Mappings
+{
+    public class SnackMap : IEntityTypeConfiguration<Snack>
+    {
+        public void Configure(EntityTypeBuilder<Snack> builder)
+        {
+            builder.ToTable("Snack");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name);
+            builder.HasMany(x => x.Ingredients);
+        }
+    }
+}
