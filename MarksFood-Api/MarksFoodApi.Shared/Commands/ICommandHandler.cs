dc6c87f4cd8642bbefd49fce3b7fd7c48af677f0
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-09-07 21:57:14 -0300

    Criação das interfaces para os commands

---
+++ MarksFood-Api/MarksFoodApi.Shared/Commands/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarksFoodApi.Shared.Commands
+{
+    public interface ICommandHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
---
+++ MarksFood-Api/MarksFoodApi.Shared/Commands/ICommandResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarksFoodApi.Shared.Commands
+{
+    public interface ICommandResult
+    {
+    }
+}
