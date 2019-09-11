2d8d6ed3840b2187f7194f1b3fe8eaced0a487b9
Lucas Marques da Silva <lmarques@viceri.com.br>
2019-09-10 22:49:46 -0300

    Remoção de métodos inutilizados na SnackController

--- MarksFood-Api/MarksFoodApi.Api/Controllers/SnackController.cs
+++ MarksFood-Api/MarksFoodApi.Api/Controllers/SnackController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using MarksFoodApi.Domain.Commands.Results;
 using MarksFoodApi.Domain.Interfaces.Services;
@@ -18,38 +16,12 @@ namespace MarksFoodApi.Api.Controllers
         {
             _snackService = snackService;
         }
-
-        // GET api/values
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SnackOutput>>> Get()
         {
             var snacks = await _snackService.GetAllSnacks();
             return Ok(snacks);
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
